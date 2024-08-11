7b249facb93686561b7dffeb206d0e40b7a96ecd
pneefe13 <patrik.neefe@gmx.de>
2024-08-11 11:37:23 +0200

    Base Command, VM

---
+++ Frontend/Commands/Command.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace Frontend.Commands
+{
+    public class Command : ICommand
+    {
+        public Predicate<object?> CanExecuteFunc { get; set; } = (p) => false;
+
+        public Action<object?> ExecuteFunc { get; set; } = (a) => {};
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return CanExecuteFunc(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            ExecuteFunc(parameter);
+        }
+    }
+}
